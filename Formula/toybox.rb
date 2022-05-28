80db94a8b30861178f535b5ba8a048efca1e07aa
Jeremy McAnally <jeremymcanally@gmail.com>
2022-05-28 12:21:38 -0400

    Update toybox.rb

--- Formula/toybox.rb
+++ Formula/toybox.rb
@@ -1,13 +1,11 @@
-class Toybox < Formula
-  cask "toybox" do
-    version "0.0.3"
-    sha256 "993d1d6133eb96315db05f50d722aac4f7b62d17"
+cask "toybox" do
+  version "0.0.3"
+  sha256 "993d1d6133eb96315db05f50d722aac4f7b62d17"
 
-    url "https://github.com/jm/toybox/releases/download/#{version}/macOS.Intel.zip"
-    name "toybox"
-    desc "Package and dependency manager for Playdate's Lua implementation"
-    homepage "https://toyboxes.io/"
+  url "https://github.com/jm/toybox/releases/download/#{version}/macOS.Intel.zip"
+  name "toybox"
+  desc "Package and dependency manager for Playdate's Lua implementation"
+  homepage "https://toyboxes.io/"
 
-    bin "toybox"
-  end
+  bin "toybox"
 end
