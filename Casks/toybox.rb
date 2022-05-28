36705fa9858e1ea318c5107b233273d91cd25d2f
Jeremy McAnally <jeremymcanally@gmail.com>
2022-05-28 12:32:42 -0400

    Update toybox.rb

--- Casks/toybox.rb
+++ Casks/toybox.rb
@@ -2,7 +2,7 @@ cask "toybox" do
   version "0.0.3"
   sha256 "993d1d6133eb96315db05f50d722aac4f7b62d17"
 
-  url "https://github.com/jm/toybox/releases/download/#{version}/macOS.Intel.zip"
+  url "https://github.com/jm/toybox/releases/download/#{version}/toybox-#{version}-darwin-amd64.zip"
   name "toybox"
   desc "Package and dependency manager for Playdate's Lua implementation"
   homepage "https://toyboxes.io/"
