1f0affe95d52493074943ed330de926a48eb9a2a
Jeremy McAnally <jeremymcanally@gmail.com>
2022-05-28 12:34:09 -0400

    Update toybox.rb

--- Casks/toybox.rb
+++ Casks/toybox.rb
@@ -1,6 +1,6 @@
 cask "toybox" do
   version "0.0.3"
-  sha256 "993d1d6133eb96315db05f50d722aac4f7b62d17"
+  sha256 "8b7510b63fb551b99e494729b0b0a6cbbd849eddea191938f8e0c75ab56f4f26"
 
   url "https://github.com/jm/toybox/releases/download/#{version}/toybox-#{version}-darwin-amd64.zip"
   name "toybox"
